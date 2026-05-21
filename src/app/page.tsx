f55c55d4500864d8e6f384e265e3543572909401
unknown <pedro.avare01@gmail.com>
2026-05-20 21:50:43 -0300

    adds seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,27 @@
 import LandingPage from '@/templates/landing-page';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Site.Set',
+  description: 'Venda seus produtos como afiliado em um único lugar',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar',
+    url: 'https://rocketseat-next-blog-i3h1.vercel.app/og-image.png',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://rocketseat-next-blog-i3h1.vercel.app/og-image.png',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
+};
 
 export default function Home() {
   return <LandingPage />;
