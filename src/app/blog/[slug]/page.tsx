79f3e1a47841823d94802d5648dc7f20d849e98b
unknown <pedro.avare01@gmail.com>
2026-05-20 22:17:09 -0300

    adds seo to blog post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,32 @@
 import PostDetail from '@/pages/blog-page/[slug]';
 import { allPosts } from 'contentlayer/generated';
+import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 interface PostDetailProps {
   params: Promise<{ slug: string }>;
+}
+
+export async function generateMetadata({
+  params,
+}: PostDetailProps): Promise<Metadata> {
+  const { slug } = await params;
+
+  const postFound = allPosts.find((post) => post.slug === slug);
+
+  if (!postFound) {
+    return {};
+  }
+
+  return {
+    title: postFound.title,
+    description: postFound.description,
+    authors: [{ name: postFound.author.name }],
+    robots: 'index, follow',
+    openGraph: {
+      images: [postFound.image],
+    },
+  };
 }
 
 export async function generateStaticParams() {
